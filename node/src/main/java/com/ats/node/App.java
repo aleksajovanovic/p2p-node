8581828874ebc53680dbc7215f4cb0a725737a87
Tiago Valverde <tiago.c.v17@gmail.com>
2019-03-31 10:37:51 -0400

    setup resources folder and read env settings from json file

--- node/src/main/java/com/ats/node/App.java
+++ node/src/main/java/com/ats/node/App.java
@@ -1,13 +1,20 @@
 package com.ats.node;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    final static String SETTINGS_FILENAME = "settings.json";
+
+    public static void main(String[] args) {
+
+        // acquire server pool ip, udp port num
+        Settings settings = new Settings(SETTINGS_FILENAME);
+
+        // init: ask for ip all dht servers, store them
+        // inform and update: add node's own pictures to network
+        // display user possible commands
+        System.out.println(settings.getServerPoolId());
+        System.out.println(settings.getUDPPort());
+        System.out.println("Hello World!");
     }
+
 }
---
+++ node/src/main/java/com/ats/node/Settings.java
@@ -0,0 +1,42 @@
+package com.ats.node;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+
+public class Settings {
+
+    private JSONObject json;
+
+    public Settings(String filename) {
+
+        try {
+            this.json = new JSONObject(readJsonFile(filename));
+
+        } catch (IOException e) {
+            System.out.println("Error reading JSON file from resources");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getServerPoolId() {
+        return this.json.getJSONObject("serverPool").getString("masterId");
+    }
+
+    public int getUDPPort() {
+        return this.json.getJSONObject("serverPool").getInt("UDPPort");
+    }
+
+    private String readJsonFile(String filename) throws IOException {
+
+        ClassLoader classLoader = App.class.getClassLoader();
+
+        try (InputStream is = classLoader.getResourceAsStream(filename)) {
+            return IOUtils.toString(is, StandardCharsets.UTF_8);
+        }
+    }
+
+}
