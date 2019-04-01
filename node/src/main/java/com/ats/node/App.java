85ff49b012b63e8d33d5787f172edccdc5ba404b
Tiago Valverde <tiago.c.v17@gmail.com>
2019-04-01 13:03:47 -0400

    add logger to main

--- node/src/main/java/com/ats/node/App.java
+++ node/src/main/java/com/ats/node/App.java
@@ -4,29 +4,42 @@ import java.net.*;
 import com.ats.node.network.udp.client.UDPClient;
 import com.ats.node.models.Peer;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App {
 
     final static String SETTINGS_FILENAME = "settings.json";
 
     public static void main(String[] args) {
 
+        NodeLogger logger = new NodeLogger();
+
         // acquire master's server pool ip, udp port
         Settings settings = new Settings(SETTINGS_FILENAME);
         String serverIP = settings.getServerPoolIp();
         int serverPort = settings.getUDPPort();
+        // log: successully acquired master ip
+        System.out.println(serverPort);
+        logger.logServerDetails(serverIP, serverPort);
 
         // init: ask for all dht servers info
+        logger.logMessage("creating \"Init\" request");
+
         Peer masterPeer = new Peer(serverIP, serverPort); // DHT master peer
         Message requestMsg = new Message("init", "");
         UDPClient udpClient = new UDPClient(masterPeer);
-        udpClient.sendPacket(requestMsg);
+        // udpClient.sendPacket(requestMsg);
+        logger.logMessage("request \"Init\" sent");
 
         // message format not defined yet
-        Message responseMsg = udpClient.receive();
+        logger.logMessage("waiting for  \"Init\" response");
+        // Message responseMsg = udpClient.receive();
+        // log: "init" received
+        logger.logMessage("\"Init\" response received");
 
         // inform and update: add node's own pictures to network
         // display user possible commands
 
     }
-
 }
