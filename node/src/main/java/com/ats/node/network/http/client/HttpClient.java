aae6f5219c0e7e4eead2fe46666f9bff6ae1927e
sauban48 <s.sakhi19@hotmail.com>
2019-04-10 17:00:23 -0400

    build

--- node/src/main/java/com/ats/node/network/http/client/HttpClient.java
+++ node/src/main/java/com/ats/node/network/http/client/HttpClient.java
@@ -10,7 +10,8 @@ public class HttpClient {
             System.out.println("HTTP URL: " + url);
             URL test = new URL("http://141.117.234.17:20410/?param1=belgrade.jpg");
 
-            HttpURLConnection con = (HttpURLConnection) (new URL(url)).openConnection();
+            HttpURLConnection con = (HttpURLConnection) test.openConnection();
+            System.out.println(con.getResponseMessage());
             int responseCode = con.getResponseCode();
             System.out.println("\nSending 'GET' request to node : " + url);
 
