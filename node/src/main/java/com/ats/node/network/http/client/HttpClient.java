ebe69ee34d2a352cdd5f448e246fde8584728a44
Tiago Valverde <tiago.c.v17@gmail.com>
2019-04-10 13:15:18 -0400

    build

--- node/src/main/java/com/ats/node/network/http/client/HttpClient.java
+++ node/src/main/java/com/ats/node/network/http/client/HttpClient.java
@@ -2,20 +2,22 @@ package com.ats.node.network.http.client;
 
 import java.io.*;
 import java.net.*;
-public class HttpClient{
 
-	public void sendGet(URL url, String file) throws Exception {
-        try{
+public class HttpClient {
+
+    public void sendGet(URL url, String file) throws Exception {
+        try {
+            System.out.println("HTTP URL: " + url);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
 
             int responseCode = con.getResponseCode();
             System.out.println("\nSending 'GET' request to node : " + url);
-            
-            if (responseCode == HttpURLConnection.HTTP_OK){
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
                 InputStream inputStream = con.getInputStream();
                 FileOutputStream outputStream = new FileOutputStream("src/main/resources/pictures/" + file);
-    
+
                 int bytesRead = -1;
                 byte[] buffer = new byte[1024];
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
@@ -23,15 +25,15 @@ public class HttpClient{
                 }
                 outputStream.close();
                 inputStream.close();
-    
+
                 System.out.println("File downloaded");
-            }else {
+            } else {
                 System.out.println("No file to download. Server replied HTTP code: " + responseCode);
             }
             con.disconnect();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-	}
+    }
 }
