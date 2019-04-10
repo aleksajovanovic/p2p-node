7b5d64e744a96c5d406c0a1aba311d8229bb061b
sauban48 <s.sakhi19@hotmail.com>
2019-04-10 15:50:43 -0400

    build

--- node/src/main/java/com/ats/node/network/http/server/ServerHandler.java
+++ node/src/main/java/com/ats/node/network/http/server/ServerHandler.java
@@ -22,7 +22,7 @@ public class ServerHandler implements HttpHandler, Runnable {
                 String response = "Sending File";
 
                 byte[] bs = response.getBytes();
-                t.sendResponseHeaders(200, bs.length);
+                t.sendResponseHeaders(200, 0);
                 OutputStream os = t.getResponseBody();
                 Files.copy(f.toPath(), os);
                 os.close();
